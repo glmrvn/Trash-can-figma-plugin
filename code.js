45e5c3f8bd4d976c15e96e420e5ee8b972dedd1a
glmrvn <glmrvn@gmail.com>
2020-11-08 19:23:10 +0300

    Check of exist the Bin page

--- code.js
+++ code.js
@@ -1,5 +1,22 @@
-// Create a Bin page 
-var a = figma.createPage()
-a.name = "🗑  Bin"
+var allPages
+var binPage
+
+// Find all pages
+var allPages = figma.root.findAll(node => node.type === "PAGE")
+
+// Checking pages names
+for (let index in allPages) {
+    let page = allPages[index];
+    if (page.name == "🗑  Bin") {
+        binPage = page
+        console.log("You already have Bin page")
+    }
+}
+
+// Create a Bin page
+if (binPage == undefined) {
+    var a = figma.createPage()
+    a.name = "🗑  Bin"
+}
 
 figma.closePlugin()
